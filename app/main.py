c6d7b05e9b148545083df50357b813fc1e9a0733
ManojKumar23-cloud <manojkumar@skylarkinfo.com>
2025-01-29 02:55:46 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, CI/CD Testing!"
+    return 'Hello, CI/CD Testing!'  #TEST
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80)
