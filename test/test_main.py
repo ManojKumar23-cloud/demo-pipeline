a774ac53155218d03b7b6adb53e6bb75f3b16731
ManojKumar23-cloud <manojkumar@skylarkinfo.com>
2025-01-29 01:12:10 +0530

    Update test_main.py

--- test/test_main.py
+++ test/test_main.py
@@ -2,10 +2,10 @@ import unittest
 from main import app
 
 class TestMain(unittest.TestCase):
-    def test_home(self):
-        tester = app.test_client(self)
-        response = tester.get('/')
-        self.assertEqual(response.status_code, 200)
+    def test_home(self):
+        tester = app.test_client()
+        response = tester.get('/')
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main()
