7b069099d7da5e237e6037ce20559738a0ac396e
ManojKumar23-cloud <manojkumar@skylarkinfo.com>
2025-01-29 00:15:27 +0530

    Update test_main.py

--- test/test_main.py
+++ test/test_main.py
@@ -3,6 +3,7 @@ from main import app
 
 class TestMain(unittest.TestCase):
     def test_home(self):
+        from main import app
         tester = app.test_client(self)
         response = tester.get('/')
         self.assertEqual(response.status_code, 200)
