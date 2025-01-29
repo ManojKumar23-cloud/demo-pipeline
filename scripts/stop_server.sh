0d556b45c8de453f4d87637f501817c7afa059e7
ManojKumar23-cloud <manojkumar@skylarkinfo.com>
2025-01-29 23:54:20 +0530

    Create stop_server.sh

---
+++ scripts/stop_server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Stopping application..."
+# Add commands to stop your application here
+# For example, if it's a Node.js app:
+# pkill node
+# Or if it's a specific process:
+# kill $(cat /path/to/pid/file)
+echo "Application stopped"
