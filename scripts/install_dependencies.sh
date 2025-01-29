1d8956f6295b95abf36c7ba903bb5fb3dde0017e
ManojKumar23-cloud <manojkumar@skylarkinfo.com>
2025-01-30 04:21:32 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 yum install -y python3 pip
 pip3 install flask
+pkill -f "python3 main.py"
+cat > /etc/httpd/conf.d/myapp.conf << 'EOF'
+<VirtualHost *:80>
+    ProxyPreserveHost On
+    ProxyPass / http://127.0.0.1:5000/
+    ProxyPassReverse / http://127.0.0.1:5000/
+
+    ErrorLog /var/log/httpd/flask-app-error.log
+    CustomLog /var/log/httpd/flask-app-access.log combined
+</VirtualHost>
+EOF
